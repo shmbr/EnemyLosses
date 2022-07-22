0ef22941fe2d608525af1d7d3ac3e961aa82ec1a
yu_shmbr <ura.shambora@gmail.com>
2022-07-22 12:06:35 +0300

    update parsing part

--- EnemyLosses/Model/EquipmentNetworkingAndParsing.swift
+++ EnemyLosses/Model/EquipmentNetworkingAndParsing.swift
@@ -15,15 +15,8 @@ func getEquipmentData(){
     }
     let task = URLSession.shared.dataTask(with: url){
         data, response, error in
-        
         if let data = data, let string = String(data: data, encoding: .utf8){
-            let json = string
-            // simulated api data
-            let data = json.data(using: .utf8)!
-            // convert to string
-            let jsString = String(data: data, encoding: .utf8)!
-            // convert back to data after replacements
-            let newData = jsString.replacingOccurrences(of: "NaN", with: "null").data(using: .utf8)!
+            let newData = string.replacingOccurrences(of: "NaN", with: "null").data(using: .utf8)!
             DispatchQueue.main.async {
                 do {
                     equimpentLossesArr = try JSONDecoder().decode([Equipment].self, from: newData)
--- EnemyLosses/Model/PersonnelNetworkingAndParsing.swift
+++ EnemyLosses/Model/PersonnelNetworkingAndParsing.swift
@@ -7,30 +7,22 @@
 
 import Foundation
 
+/// output arr
 var personnelLossesArr: Array<Personnel> = []
 
 func getPersonnelData(){
+    
     guard let url = URL(string: "https://raw.githubusercontent.com/MacPaw/2022-Ukraine-Russia-War-Dataset/main/data/russia_losses_personnel.json") else{
         return
     }
+    
     let task = URLSession.shared.dataTask(with: url){
         data, response, error in
-        
         if let data = data, let string = String(data: data, encoding: .utf8){
-
-            //print(string)
-            let json = string
-            // simulated api data
-            let data = json.data(using: .utf8)!
-            // convert to string
-            let jsString = String(data: data, encoding: .utf8)!
-            // convert back to data after replacements
-            let newData = jsString.replacingOccurrences(of: "NaN", with: "null").data(using: .utf8)!
+            let newData = string.replacingOccurrences(of: "NaN", with: "null").data(using: .utf8)!
             DispatchQueue.main.async {
                 do {
                     personnelLossesArr = try JSONDecoder().decode([Personnel].self, from: newData)
-                    //print("pers data loaded")
-                    
                 } catch (let error) {
                     print("\n---> error: \(error)")
                 }
